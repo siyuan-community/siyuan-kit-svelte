3d8e9c8e0a36467f82e18048458b62b1af06a60e
Luto Yvan <zuoez02@outlook.com>
2023-12-12 23:10:39 +0800

    v0.0.3

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,14 @@
 // Reexport your entry components here
+// Atomic component
 import Button from './Button.svelte';
 import Input from './Input.svelte';
 import Switch from './Switch.svelte';
 import Slider from './Slider.svelte';
 import Select from './Select.svelte';
+import Textarea from './Textarea.svelte';
+import Icon from './Icon.svelte';
 
-export { Button, Input, Slider, Select, Switch };
+// High level component
+import Tree from './Tree.svelte';
+
+export { Button, Input, Slider, Select, Switch, Textarea, Icon, Tree };
