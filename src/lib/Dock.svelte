5f9f3987232cc78259ab98188ae711e1460b4845
zuoez02 <zuoez02@outlook.com>
2024-01-24 11:34:58 +0800

    v0.1.2: Fix Dock logo for v2.12.4

--- src/lib/Dock.svelte
+++ src/lib/Dock.svelte
@@ -20,7 +20,7 @@
 <div class={'fn__flex-1 fn__flex-column ' + name}>
 	<div class="block__icons">
 		<div class="block__logo">
-			<svg><use xlink:href={dockIcon} /></svg>
+			<svg class="block__logoicon"><use xlink:href={dockIcon} /></svg>
 			{title}
 		</div>
 		<span class="fn__flex-1 fn__space" />
